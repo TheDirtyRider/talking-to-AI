926221632034d46b22b38b16ab768ed34dc5b957
TheDirtyRider <129997177+TheDirtyRider@users.noreply.github.com>
2023-04-05 16:11:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Speech recognition test</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
-  <body>
-    <h1>Speech recognition test</h1>
-    <p>Press the button and speak into your microphone:</p>
-    <button id="mic-btn">Speak</button>
-    <input type="text" id="input-field">
-    <script src="script.js"></script>
-  </body>
-</html>
+var recognition = new webkitSpeechRecognition();
+
+recognition.addEventListener("result", function(event) {
+  var transcript = event.results[0][0].transcript;
+  var inputField = document.getElementById("input-field");
+  inputField.value = transcript;
+});
+
+document.getElementById("mic-btn").addEventListener("click", function() {
+  recognition.start();
+});
