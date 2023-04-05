17d3f19f2684a014db9c54c0f8189ed66ac66330
TheDirtyRider <129997177+TheDirtyRider@users.noreply.github.com>
2023-04-05 18:15:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
-var recognition = new webkitSpeechRecognition();
+const recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
 
-recognition.addEventListener("result", function(event) {
-  var transcript = event.results[0][0].transcript;
-  var inputField = document.getElementById("input-field");
-  inputField.value = transcript;
-});
+const inputField = document.getElementById("input-field");
+const micBtn = document.getElementById("mic-btn");
+const micStatus = document.getElementById("mic-status");
+const historyContainer = document.getElementById("history-container");
 
-document.getElementById("mic-btn").addEventListener("click", function() {
-  recognition.start();
+let isRecording = false;
+
+recognition.onresult = function(event) {
+  let finalTranscript = "";
+  for (let i = event.resultIndex; i < event.results.length; i++) {
+    const transcript = event.results[i][0].transcript;
+    if (event.results[i].isFinal) {
+      finalTranscript += transcript;
+    } else {
+      inputField.value = finalTranscript + transcript;
+    }
+  }
+  inputField.value = finalTranscript;
+  historyContainer.innerHTML += `<p class="message">${finalTranscript}</p>`;
+};
+
+recognition.onstart = function() {
+  micBtn.classList.add("recording");
+  micStatus.classList.add("recording");
+};
+
+recognition.onend = function() {
+  micBtn.classList.remove("recording");
+  micStatus.classList.remove("recording");
+};
+
+micBtn.addEventListener("click", function() {
+  if (!isRecording) {
+    isRecording = true;
+    recognition.start();
+    micBtn.style.backgroundColor = "#f44336"; // red
+  } else {
+    isRecording = false;
+    recognition.stop();
+    micBtn.style.backgroundColor = "#4CAF50"; // green
+  }
 });
