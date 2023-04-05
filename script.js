9d51513b3decbd12e94a18ae162ceab24c5773bf
TheDirtyRider <129997177+TheDirtyRider@users.noreply.github.com>
2023-04-05 15:48:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Speech recognition test</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <h1>Speech recognition test</h1>
+    <p>Press the button and speak into your microphone:</p>
+    <button id="mic-btn">Speak</button>
+    <input type="text" id="input-field">
+    <script src="script.js"></script>
+  </body>
+</html>
