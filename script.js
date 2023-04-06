c81b3a7845c35ca7ec66d5fadccc36b510eaf2c8
TheDirtyRider <129997177+TheDirtyRider@users.noreply.github.com>
2023-04-06 03:34:35 -0500

    Add files via upload
    This is a working "gen 1" file set.

---
+++ script.js
@@ -0,0 +1,11 @@
+var recognition = new webkitSpeechRecognition();
+
+recognition.addEventListener("result", function(event) {
+  var transcript = event.results[0][0].transcript;
+  var inputField = document.getElementById("input-field");
+  inputField.value = transcript;
+});
+
+document.getElementById("mic-btn").addEventListener("click", function() {
+  recognition.start();
+});
